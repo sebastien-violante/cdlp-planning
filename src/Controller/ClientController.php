3013242f0ec6aa2df7d57874fa0b1f97acdebe9b
sebastien-violante <sebastien.violante@gmail.com>
2023-08-17 11:17:48 +0200

    mail improvment

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -7,10 +7,12 @@ use App\Form\ClientType;
 use App\Services\MailerService;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use App\Repository\ClientRepository;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\Transport;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,24 +33,29 @@ class ClientController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $client->setCleaned(false);
-            $fromEmail = 'sebastien.violante@gmail.com';
-        $toEmail = 'sebastien.violante@gmail.com';
-        $email = (new Email())
-        ->from($fromEmail)
-        ->to($toEmail)
-        ->subject('Corniche de la plage : nouvelle réservation')
-        ->text('hello')
-        ->html('<h3>Salut mon service de mail</h3>');
-        //->context([
-        //'client' => $client,
-        //]);
-        $transport = Transport::fromDsn($_ENV['MAILER_DSN']);
-        $mailer = new Mailer($transport);
-        $mailer->send($email);
+            // MAILING
+            $transport = Transport::fromDsn($_ENV['MAILER_DSN']);
+            $mailer = new Mailer($transport);
+            $toEmail = 'sebastien.violante@gmail.com';
+            
+            $email = (new TemplatedEmail())
+            ->from(new Address('bastien.c@dev-uptoyou.fr', 'Appartement Bénodet'))
+            ->to($toEmail)
+            //->cc($ccEmail)
+            ->subject('Corniche de la plage : nouvelle réservation')
+            ->html('
+            <h3>Nouvelle réservation</h3>
+            <hr>
+            <p>L\'appartement de Bénodet vient d\'être réservé par <strong>'.$client->getFirstname().'</strong> du '.$client->getArrivalDate()->format('j l Y').' au '.$client->getDepartureDate()->format('j l Y').'.</p>
+            <p>Pensez à consulter le calendrier à l\'adresse : <i>https://cdlp.dev-uptoyou.fr</i></p>
+            ')
+            ;
+            
+            $mailer->send($email);
             // persisting new client
             $entityManagerInterface->persist($client);
             $entityManagerInterface->flush();
-                     
+            
         }
 
         return $this->render('client/index.html.twig', [
