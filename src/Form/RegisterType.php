fdb71d371f2b83aeee394a534ea6666557275d9a
sebastien-violante <sebastien.violante@gmail.com>
2023-08-24 14:16:33 +0200

    role hierarchy

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,14 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -30,6 +31,13 @@ class RegisterType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Confirmer le mot de passe'],
             ])
+            ->add('roles', ChoiceType::class, [
+                    'choices' => ['Administrateur' => 'ROLE_ADM', 'Utilisateur' => 'ROLE_USER'],
+                    'required' => true,
+                    'multiple' => true,
+                    'expanded' => true,
+                ]
+            )
             ->add('submit', SubmitType::class, [
                 'label' => "Inscrire le nouvel utilisateur"
             ])
