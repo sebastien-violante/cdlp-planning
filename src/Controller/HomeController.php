c0cb1c3319a438d286182fe00d4ac5d3f112dff7
sebastien-violante <sebastien.violante@gmail.com>
2023-09-23 22:23:41 +0200

    add days before arrival

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Datetime;
 use App\Repository\ClientRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * class HomeController allows to render the list of rentals
@@ -25,9 +26,10 @@ class HomeController extends AbstractController
     {
         // All the clients are displayed but sorted by arrival date, thanks to the findBySortedAvvilDate method in the ClientRepository
         $clients = $clientRepository->findBySortedArrivalDate();
-        
+        $dateNow = new \Datetime();    
         return $this->render('home/index.html.twig', [
             'clients' => $clients,
+            'dateNow' => $dateNow,
         ]);
     }
 }
